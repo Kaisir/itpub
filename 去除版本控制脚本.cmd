56cecb925fc32aab01b2886eecbdf35fd36679e9
luziwei <loseway216@gmail.com>
2019-02-13 15:52:49 +0800

    'initialize'

--- 去除版本控制脚本.cmd
+++
@@ -1,9 +0,0 @@
-@echo on
-
-@rem É¾³ýSVN°æ±¾¿ØÖÆÄ¿Â¼
-
-@rem for /r . %%a in (.) do @if exist "%%a\.svn" @echo "%%a\.svn"
-@for /r . %%a in (.) do @if exist "%%a\.svn" rd /s /q "%%a\.svn"
-
-@echo completed
-@pause
